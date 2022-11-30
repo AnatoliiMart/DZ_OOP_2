1897055a62c69487c67096d1bf98fd9176729bd2
Anatolii <99074480+AnatoliiMart@users.noreply.github.com>
2022-11-30 20:58:56 +0200

    Rarest Version

---
+++ Source.cpp
@@ -0,0 +1,73 @@
+#include <iostream>
+
+
+class PhoneBook
+{
+public:
+	PhoneBook()
+	{
+		_initials			 = nullptr;
+		_homePhone			 = nullptr;
+		_mobilePhone	   = nullptr;
+		_additionalInfo = nullptr;
+	}
+	PhoneBook(const char* Initials, const char* HomePhone, const char* MobilePhone, const char* AdditionalInfo)
+	{
+		_initials = new char[strlen(Initials + 1)];
+		strcpy_s(_initials,  strlen(Initials + 1), Initials);
+
+		_homePhone = new char[strlen(HomePhone + 1)];
+		strcpy_s(_homePhone,  strlen(HomePhone + 1), HomePhone);
+
+		_mobilePhone = new char[strlen(MobilePhone + 1)];
+		strcpy_s(_mobilePhone,  strlen(MobilePhone + 1), MobilePhone);
+
+		_additionalInfo = new char[strlen(AdditionalInfo + 1)];
+		strcpy_s(_additionalInfo,  strlen(AdditionalInfo + 1), AdditionalInfo);
+	}
+	~PhoneBook()
+	{
+		delete[] _initials;
+		delete[] _homePhone;
+		delete[] _mobilePhone;
+		delete[] _additionalInfo;
+	}
+	void InputData()
+	{
+		std::cout << "Enter the new contact: " << std::endl;
+
+		std::cout << "Initials: ";
+		std::cin  >> _initials;
+		std::cout << std::endl;
+
+		std::cout << "Home phone: ";
+		std::cin  >> _homePhone;
+		std::cout << std::endl;
+
+		std::cout << "Mobile phone: ";
+		std::cin  >> _mobilePhone;
+		std::cout << std::endl;
+
+		std::cout << "Additional infotmation: ";
+		std::cin  >> _additionalInfo;
+		std::cout << std::endl;
+	}
+	void Output()
+	{
+		if (true)
+		{
+
+		}
+	}
+
+private:
+	char* _initials;
+	char* _homePhone;
+	char* _mobilePhone;
+	char* _additionalInfo;
+};
+
+int main()
+{
+
+}
